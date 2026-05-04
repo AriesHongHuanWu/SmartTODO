05c060f6baefbec8a6b5282a8056201fa85b9c3d
吳泓寰 <arieswu001@gmail.com>
2026-05-04 17:48:22 +0800

    Update extension manifest and vite config

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         popup: resolve(__dirname, 'popup.html'),
